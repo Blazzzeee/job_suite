3a00901336eb712fe74285f250e6f3b9f604a9d3
Blazzzeee <maanikhurana1007@gmail.com>
2025-08-31 22:56:00 +0530

    dockerise client and server with compose

--- api.py
+++ api.py
@@ -232,4 +232,4 @@ async def job_logs_ws(websocket: WebSocket, job_id: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
